78b3ac7d1cd263a86661c1f2d20e399f2b3a28b7
Rizhik <venera124@yandex.ru>
2015-01-06 15:17:24 -0800

    Added project files and folders

---
+++ src/main/java/org/perova/TicTacToe/App.java
@@ -0,0 +1,13 @@
+package org.perova.TicTacToe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
