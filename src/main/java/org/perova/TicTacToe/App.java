a9a8ae1efd84005bb61f9a6858d9527bb6950926
Rizhik <venera124@yandex.ru>
2015-01-20 20:59:04 -0800

    Following functions were re-designed: - checkCrossesForWinner - checkRowsforWinner - checkColumnsForWinner
    Following function was added:
    - goTo - this function helps checkCrossesForWinner function navigate
    through the field. If step = 1, function goes to the RIGHT, if step =
    -1, function goes to the LEFT.
    
    Fixed defects:
    - TicTacFunction shows wrong winner

--- src/main/java/org/perova/TicTacToe/App.java
+++ src/main/java/org/perova/TicTacToe/App.java
@@ -24,87 +24,118 @@ public class App {
 		}
 	}
 
-	protected int checkCrossesForWiner(int[][] field) {
-		boolean flag = true;
+	protected int goTo(int[][] field, int currentRow, int currentColumn,
+			int step) {
+		int numberOfElements = 0;
+		int currentElement = 0;
 
-		for (int row = 0, column = 0; row < 2 && column < 2; row++, column++) {
-			if (field[row][column] != field[row + 1][column + 1]) {
-				flag = false;
+		if (field[currentRow][currentColumn] != 0) {
+			currentElement = field[currentRow][currentColumn];
+			numberOfElements++;
+
+			int stepCounter = 1;
+			while (stepCounter != 3) {
+				if (((currentRow + 1) < field.length)
+						&& (currentColumn + step >= 0)
+						&& ((currentColumn + step) < field[currentRow].length)
+						&& (currentElement == field[currentRow + 1][currentColumn
+								+ step])) {
+					numberOfElements++;
+
+				} else {
+					break;
+				}
+				stepCounter++;
 			}
-		}
 
-		if (flag == true) {
-			System.out.println("Winer is " + field[0][0]);
-			return field[0][0];
-		}
-		
-		flag = true;
-
-		for (int row = 2, column = 0; row >= 0 && column < 2; row--, column++) {
-			if (field[row][column] != field[row - 1][column + 1]) {
-				flag = false;
+			if (numberOfElements == 3) {
+				return currentElement;
 			}
-		}
 
-		if (flag == true) {
-			System.out.println("Winer is " + field[0][2]);
-			return field[0][2];
 		}
-
 		return 0;
 	}
 
-	protected int checkRawsForWinner(int[][] field) {
-		int numberOfOne = 0;
-		int numberOfTwo = 0;
-		for (int raw = 0; raw < field.length; raw++) {
-			for (int column = 0; column < field[raw].length; column++) {
-				if (field[raw][column] == 1) {
-					numberOfOne++;
-				}
-				if (field[raw][column] == 2) {
-					numberOfTwo++;
-				}
-			}
-			if (numberOfOne == field.length) {
-				System.out.println("Winer is 1");
-				return 1;
-			}
+	protected int checkCrossesForWiner(int[][] field) {
 
-			if (numberOfTwo == field.length) {
-				System.out.println("Winer is 2");
-				return 2;
+		for (int row = 0; row < field.length; row++) {
+			for (int column = 0; column < field[row].length; column++) {
+
+				// Go right
+				int rightResult = goTo(field, row, column, 1);
+
+				if (rightResult != 0) {
+					return rightResult;
+				}
+				// Go left
+				int leftResult = goTo(field, row, column, -1);
+
+				if (leftResult != 0) {
+					return leftResult;
+				}
 			}
-			numberOfOne = 0;
-			numberOfTwo = 0;
+		}
+		return 0;
+	}
+
+	protected int checkRowsForWinner(int[][] field) {
+		int numberOfElements = 0;
+		int currentElement = 0;
+		for (int row = 0; row < field.length; row++) {
+			for (int column = 0; column < field[row].length; column++) {
+
+				if (currentElement != 0 && currentElement == field[row][column]) {
+
+					numberOfElements++;
+
+					if (numberOfElements == 3) {
+
+						return currentElement;
+
+					}
+				}
+
+				if (currentElement == 0 && field[row][column] != 0) {
+
+					currentElement = field[row][column];
+					numberOfElements++;
+
+				}
+
+			}
+			currentElement = 0;
+			numberOfElements = 0;
 		}
 		return 0;
 	}
 
 	protected int checkColumnsForWinner(int[][] field) {
-		int numberOfOne = 0;
-		int numberOfTwo = 0;
-		for (int column = 0; column < 3; column++) { // need to find solution
-														// for 3
-			for (int raw = 0; raw < field.length; raw++) {
-				if (field[raw][column] == 1) {
-					numberOfOne++;
-				}
-				if (field[raw][column] == 2) {
-					numberOfTwo++;
-				}
-			}
-			if (numberOfOne == field.length) {
-				System.out.println("Winer is 1");
-				return 1;
-			}
+		int numberOfElements = 0;
+		int currentElement = 0;
 
-			if (numberOfTwo == field.length) {
-				System.out.println("Winer is 2");
-				return 2;
+		for (int column = 0; column < field[0].length; column++) {
+			for (int row = 0; row < field.length; row++) {
+				if (currentElement != 0 && currentElement == field[row][column]) {
+
+					numberOfElements++;
+
+					if (numberOfElements == 3) {
+
+						return currentElement;
+
+					}
+				}
+
+				if (currentElement == 0 && field[row][column] != 0) {
+
+					currentElement = field[row][column];
+					numberOfElements++;
+
+				}
+
 			}
-			numberOfOne = 0;
-			numberOfTwo = 0;
+			currentElement = 0;
+			numberOfElements = 0;
 		}
 		return 0;
 	}
@@ -116,25 +147,35 @@ public class App {
 			game.printField(field);
 
 			// Check crosses for winer
-			if (game.checkCrossesForWiner(field) == 0) {
+			int crossesWinner = game.checkCrossesForWiner(field);
+			if (crossesWinner != 0) {
+				System.out.println("Winner is " + crossesWinner);
+			}
 
-				// Check raws for winner
-				if (game.checkRawsForWinner(field) == 0) {
+			// Check rows for winner
+			int rowWinner = game.checkRowsForWinner(field);
+			if (rowWinner != 0) {
+				System.out.println("Winner is " + rowWinner);
+			}
 
-					// Check columns for winner
-					if (game.checkColumnsForWinner(field) == 0) {
+			// Check columns for winner
+			int columnWinner = game.checkColumnsForWinner(field);
+			if (columnWinner != 0) {
 
-						System.out.println("There is no winer!");
+				System.out.println("Winner is " + columnWinner);
 
-					}
-				}
+			}
+
+			// There is no winner message
+			if (crossesWinner == 0 && rowWinner == 0 && columnWinner == 0) {
+				System.out.println("There is no winner!!!");
 			}
 		}
 	}
 
 	public static void main(String[] args) {
 		App myGame = new App();
-		int[][] field = new int[][] { { 0, 1, 1 }, { 2, 1, 0 }, { 1, 0, 2 } };
+		int[][] field = new int[][] { { 2, 0, 1 }, { 2, 1, 1 }, { 1, 1, 2 } };
 		myGame.TicTacToe(field);
 	}
 }
